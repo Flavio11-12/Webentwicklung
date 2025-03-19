50943da8c23b8779656887a8f81d6e3e1126f7e9
Flavio11-12 <11flacar@gmail.com>
2025-03-19 12:28:24 +0100

    readme update

--- Spiel/script.js
+++ Spiel/script.js
@@ -1,5 +1,22 @@
 const button = document.getElementById('button');
-const Überschrift = document.getElementById('Überschrift');
+const button5 = document.getElementById('button5');
+const button10 = document.getElementById('button10');
+const button25 = document.getElementById('button25');
+const button50 = document.getElementById('button50');
+const button100 = document.getElementById('button100');
+const Währung_anzeige = document.getElementById('Währung');
+var Währung = 0;
 button.addEventListener('click', () => {
-    Überschrift.style.display = "none";
+});
+button5.addEventListener('click', () => {
+    Währung += 5;
+    Währung_anzeige.innerHTML = Währung + "€";
+});
+button10.addEventListener('click', () => {
+    Währung += 10;
+    Währung_anzeige.innerHTML = Währung + "€";
+});
+button25.addEventListener('click', () => {
+    Währung += 25;
+    Währung_anzeige.innerHTML = Währung + "€";
 });
