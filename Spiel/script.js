74b4f1e9f5831b51d9e54df332a37133be59c486
Flavio11-12 <11flacar@gmail.com>
2025-03-20 10:13:56 +0100

    readme update

--- Spiel/script.js
+++ Spiel/script.js
@@ -5,6 +5,8 @@ const button25 = document.getElementById('button25');
 const button50 = document.getElementById('button50');
 const button100 = document.getElementById('button100');
 const Währung_anzeige = document.getElementById('Währung');
+const Ergebnis_anzeige = document.getElementById('Ergebnis');
+var Ergebnis;
 var Währung = 0;
 button.addEventListener('click', () => {
 });
@@ -19,4 +21,19 @@ button10.addEventListener('click', () => {
 button25.addEventListener('click', () => {
     Währung += 25;
     Währung_anzeige.innerHTML = Währung + "€";
+});
+
+button.addEventListener('click', () => {
+    Ergebnis = Math.floor(Math.random() * 3);
+    switch (Ergebnis) {
+        case 0:
+            Ergebnis_anzeige.innerHTML = '<img src="Bilder/schere.jpg" alt="Ergebnis" width="100">';
+            break;
+        case 1:
+            Ergebnis_anzeige.innerHTML = '<img src="Bilder/TheRock.jpeg" alt="Ergebnis" width="100">';
+            break;
+        case 2:
+            Ergebnis_anzeige.innerHTML = '<img src="Bilder/Papier.jpg" alt="Ergebnis" width="100">';
+            break;
+    }
 });
